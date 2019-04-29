8462e66799bd34ce9adcac885314f506cc259cc6
beauhansen <beau.hansen45@gmail.com>
2019-04-28 20:41:16 -0600

    Added files for Udemy course Section 2

---
+++ UdemyCourseIntroToR/YourFirstRSession/BasicCommandsAssignment.R
@@ -0,0 +1,22 @@
+#Basic Commands: Assignment
+#1. Assignment
+a <- 3
+A = 5
+a
+A
+b <- 6
+b <- b + 5
+c <- 7
+c <- "China"
+
+#3. Assignment
+d    <-  8
+e <- 8
+var <- 99
+f <- .Last.value  
+
+#4. Comments
+#This is a comment
+g <- 80 #this is a comment at the end of a line
+g <- #this is a comment over a multi line statement
+	81
